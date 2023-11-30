2ed60e36e4b4c3f16b4cbed75e7dabfb42d66ef7
Gustavo Albuquerque <albuquerque.develop@gmail.com>
2023-11-30 12:13:13 -0300

    Finaliza página de relatório

--- service/api.js
+++ service/api.js
@@ -2,7 +2,9 @@ class Api {
     rotas = {
         healthCheck: { rota: `http://localhost:3000/carrinho/healthCheck`, metodo: 'GET' },
         adicionarItemAoCarrinho: { rota: `http://localhost:3000/carrinho`, metodo: 'POST' },
-        removerItemDoCarrinho: { rota: `http://localhost:3000/carrinho`, metodo: 'DELETE' }
+        finalizarCompra: { rota: `http://localhost:3000/carrinho/finalizar-compra`, metodo: 'POST' },
+        removerItemDoCarrinho: { rota: `http://localhost:3000/carrinho`, metodo: 'DELETE' },
+        relatorio: { rota: `http://localhost:3000/carrinho/relatorio`, metodo: 'GET' }
     }
     async healthCheck() {
         console.log('API >> healthCheck')
@@ -35,18 +37,58 @@ class Api {
             console.error(error)
         }
     }
-    async removerItemDoCarrinho(nome_item) {
-        console.log('API >> adicionarItemAoCarrinho')
+    async removerItemDoCarrinho(nome) {
+        console.log('API >> removerItemDoCarrinho')
         try {
-            await fetch(this.rotas.adicionarItemAoCarrinho.rota + `/${nome_item}`,
+            
+            await fetch(`${this.rotas.removerItemDoCarrinho.rota}?` + new URLSearchParams({
+                nome
+            }).toString(),
                 {
                     method: this.rotas.removerItemDoCarrinho.metodo,
                     headers: {
                         "Content-Type": "application/json",
                     },
+                    
                 })
 
             return true
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    async finalizarCompra() {
+        console.log('API >> finalizarCompra')
+        try {
+            const response = await fetch(this.rotas.finalizarCompra.rota,
+                {
+                    method: this.rotas.finalizarCompra.metodo,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+
+            const finalizarCompra = await response.json()
+
+            return finalizarCompra
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    async relatorio() {
+        console.log('API >> relatorio')
+        try {
+            const response = await fetch(this.rotas.relatorio.rota,
+                {
+                    method: this.rotas.relatorio.metodo,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+
+            const relatorio = await response.json()
+
+            return relatorio
         } catch (error) {
             console.error(error)
         }
